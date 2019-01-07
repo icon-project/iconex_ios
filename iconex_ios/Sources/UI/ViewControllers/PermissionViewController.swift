c801985b7a752498d22caebc9009d80d60d530dd
a1ahn <jeonghwan.ahn@icon.foundation>
2019-01-07 12:04:53 +0900

    Add localized strings

---
+++ iconex_ios/Sources/UI/ViewControllers/PermissionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PermissionViewController.swift
+//  iconex_ios
+//
+//  Created by a1ahn on 04/01/2019.
+//  Copyright © 2019 ICON Foundation. All rights reserved.
+//
+
+import UIKit
+
+class PermissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
