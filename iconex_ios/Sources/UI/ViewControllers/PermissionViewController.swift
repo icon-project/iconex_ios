0bb312ba73e7792ad3e59770512af559e1577fbd
a1ahn <jeonghwan.ahn@icon.foundation>
2019-01-07 15:46:12 +0900

    Add permission view

--- iconex_ios/Sources/UI/ViewControllers/PermissionViewController.swift
+++ iconex_ios/Sources/UI/ViewControllers/PermissionViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 import RxSwift
 
 class PermissionViewController: BaseViewController {
+    @IBOutlet weak var permissionTitle: UILabel!
+    @IBOutlet weak var requiredTitle: UILabel!
+    @IBOutlet weak var requiredDesc: UILabel!
+    @IBOutlet weak var optionalTitle: UILabel!
+    @IBOutlet weak var optionalCameraTitle: UILabel!
+    @IBOutlet weak var optionalCameraDesc: UILabel!
+    @IBOutlet weak var footerDesc1: UILabel!
+    @IBOutlet weak var footerDesc2: UILabel!
     @IBOutlet weak var confirmButton: UIButton!
     
     override func viewDidLoad() {
@@ -33,6 +41,14 @@ class PermissionViewController: BaseViewController {
     }
     
     func initializeUI() {
+        permissionTitle.text = "Permission.Title".localized
+        requiredTitle.text = "Permission.Required.Title".localized
+        requiredDesc.text = "Permission.None".localized
+        optionalTitle.text = "Permission.Optional.Title".localized
+        optionalCameraTitle.text = "Permission.Camera.Title".localized
+        optionalCameraDesc.text = "Permission.Camera.Desc".localized
+        footerDesc1.text = "Permission.footer.desc1".localized
+        footerDesc2.text = "Permission.footer.desc2".localized
         confirmButton.setTitle("Common.Confirm".localized, for: .normal)
         confirmButton.styleDark()
         confirmButton.rounded()
