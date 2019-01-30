1b2b021673a79c9928d54ed39d7810bd17ea4259
a1ahn <jeonghwan.ahn@icon.foundation>
2019-01-30 11:34:12 +0900

    Add Keystore

---
+++ iconex_ios/Sources/ICONex/Wallet/WalletKeystore.swift
@@ -0,0 +1,134 @@
+//
+//  WalletKeystore.swift
+//  iconex_ios
+//
+//  Copyright © 2019 ICON Foundation. All rights reserved.
+//
+
+import Foundation
+import ICONKit
+import CryptoSwift
+import scrypt
+
+public class Keystore: Codable {
+    public var version: Int = 3
+    public var id: String = UUID().uuidString
+    public var address: String
+    public var crypto: Crypto
+    public var coinType: String = "icx"
+    
+    enum KeystoreCodingKey: String, CodingKey {
+        case version
+        case id
+        case address
+        case crypto
+        case Crypto
+        case coinType
+    }
+    
+    init(address: String, crypto: Crypto) {
+        self.address = address
+        self.crypto = crypto
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: KeystoreCodingKey.self)
+        
+        self.version = try container.decode(Int.self, forKey: .version)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.address = try container.decode(String.self, forKey: .address)
+        if container.contains(.crypto) {
+            self.crypto = try container.decode(Crypto.self, forKey: .crypto)
+        } else {
+            self.crypto = try container.decode(Crypto.self, forKey: .Crypto)
+        }
+    }
+    
+    public struct Crypto: Codable {
+        public var ciphertext: String
+        public var cipherparams: CipherParams
+        public var cipher: String
+        public var kdf: String
+        public var kdfparams: KDF
+        public var mac: String
+    }
+    
+    public struct CipherParams: Codable {
+        public var iv: String
+    }
+    
+    public struct KDF: Codable {
+        public let dklen: Int
+        public var salt: String
+        public var c: Int?
+        public var n: Int?
+        public var p: Int?
+        public var r: Int?
+        public let prf: String?
+        
+        init(dklen: Int, salt: String, c: Int, prf: String) {
+            self.dklen = dklen
+            self.salt = salt
+            self.c = c
+            self.prf = prf
+        }
+    }
+    
+    func isValid(password: String) throws -> Bool {
+        if self.crypto.kdf == "pbkdf2" {
+            guard let enc = self.crypto.ciphertext.hexToData(),
+                let iv = self.crypto.cipherparams.iv.hexToData(),
+                let salt = self.crypto.kdfparams.salt.hexToData(),
+                let count = self.crypto.kdfparams.c else { throw IXError.invalidKeystore }
+            
+            guard let devKey = Cipher.pbkdf2SHA256(password: password, salt: salt, keyByteCount: PBE_DKLEN, round: count) else { throw IXError.decrypt }
+            
+            let decrypted = try Cipher.decrypt(devKey: devKey, enc: enc, dkLen: PBE_DKLEN, iv: iv)
+            let prvKeyStr = decrypted.decryptText
+            let privateKey = PrivateKey(hexData: prvKeyStr.hexToData()!)
+            
+            let wallet = Wallet(privateKey: privateKey)
+            let newAddress = wallet.address
+            
+            if newAddress == self.address {
+                return true
+            }
+            
+            throw IXError.decrypt
+            
+        } else if self.crypto.kdf == "scrypt" {
+            guard let n = self.crypto.kdfparams.n,
+                let p = self.crypto.kdfparams.p,
+                let r = self.crypto.kdfparams.r,
+                let iv = self.crypto.cipherparams.iv.hexToData(),
+                let cipherText = self.crypto.ciphertext.hexToData(),
+                let salt = self.crypto.kdfparams.salt.hexToData()
+                else { throw IXError.keyMalformed }
+            
+            guard let devKey = Cipher.scrypt(password: password, saltData: salt, dkLen: self.crypto.kdfparams.dklen, N: n, R: r, P: p) else { throw IXError.decrypt }
+            let decryptionKey = devKey[0...15]
+            guard let aesCipher = try? AES(key: decryptionKey.bytes, blockMode: CTR(iv: iv.bytes), padding: .noPadding) else { throw IXError.decrypt }
+            guard let decryptedBytes = try? aesCipher.decrypt(cipherText.bytes) else { throw IXError.decrypt }
+            let decrypted = Data(bytes: decryptedBytes)
+            let prvKey = PrivateKey(hexData: decrypted)
+            let wallet = Wallet(privateKey: prvKey)
+            let newAddress = wallet.address
+            
+            if newAddress == self.address {
+                return true
+            }
+            
+            throw IXError.invalidPassword
+        }
+        
+        throw IXError.invalidKeystore
+    }
+    
+    func toString() -> String {
+        let encoder = JSONEncoder()
+        
+        let data = try! encoder.encode(self)
+        
+        return String(data: data, encoding: .utf8)!.replacingOccurrences(of: "\\", with: "")
+    }
+}
