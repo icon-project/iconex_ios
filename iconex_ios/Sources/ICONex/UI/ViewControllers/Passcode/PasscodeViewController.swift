7914c3cfd07c0acc4713435c25d6a0299b6a8759
a1ahn <a1ahn@iconloop.com>
2019-07-31 17:04:12 +0900

    Add Passcode

---
+++ iconex_ios/Sources/ICONex/UI/ViewControllers/Passcode/PasscodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PasscodeViewController.swift
+//  iconex_ios
+//
+//  Created by a1ahn on 31/07/2019.
+//  Copyright © 2019 ICON Foundation. All rights reserved.
+//
+
+import UIKit
+
+class PasscodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
