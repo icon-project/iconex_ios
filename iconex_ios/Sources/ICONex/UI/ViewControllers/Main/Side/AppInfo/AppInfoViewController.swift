bd48781a5fafa6a02e8648b81a467a9398bf20a2
a1ahn <a1ahn@iconloop.com>
2019-09-01 15:59:37 +0900

    Add App Infos

---
+++ iconex_ios/Sources/ICONex/UI/ViewControllers/Main/Side/AppInfo/AppInfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AppInfoViewController.swift
+//  iconex_ios
+//
+//  Created by a1ahn on 01/09/2019.
+//  Copyright © 2019 ICON Foundation. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class AppInfoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func initializeComponents() {
+        super.initializeComponents()
+    }
+}
